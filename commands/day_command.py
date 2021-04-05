fc48caf8edb4b8073362dc0a5e80ebd0079619a1
yakninja <andrey@yakninja.pro>
2021-04-05 19:14:01 +0200

    refactoring, day stats

--- commands/day_command.py
+++ commands/day_command.py
@@ -12,6 +12,7 @@ from models.user import get_or_create_user
 def day_command(update: Update, _: CallbackContext) -> None:
     strings = []
     user = get_or_create_user(update.message.from_user.id)
+    profile = user.profile
     food_logs = db_session.query(FoodLog) \
         .filter_by(user_id=user.id, date=date_now()) \
         .order_by(FoodLog.created_at) \
@@ -27,17 +28,37 @@ def day_command(update: Update, _: CallbackContext) -> None:
             i18n.t('Carbs'),
             i18n.t('Protein')))
 
+    calories_left = profile.daily_calories
+    fat_left = profile.daily_fat
+    carbs_left = profile.daily_carbs
+    protein_left = profile.daily_protein
     for fl in food_logs:
         food = fl.food
         food_name = db_session.query(FoodName).filter_by(food_id=food.id).first()
         name = food_name.name if food_name else '?'
-        strings.append('{}\t{}\t{}\t{}\t{}\t{}'.format(
+        strings.append('{}\t{}\t{:.0f}\t{:.2f}\t{:.2f}\t{:.2f}'.format(
             datetime.utcfromtimestamp(fl.created_at).strftime('%H:%M'),
             name,
             fl.calories,
             fl.fat,
             fl.carbs,
             fl.protein))
+        calories_left -= fl.calories
+        fat_left -= fl.fat
+        carbs_left -= fl.carbs
+        protein_left -= fl.protein
 
     strings.append('')
+
+    strings.append(i18n.t('Daily remainder:'))
+    strings.append('{}\t{}\t{}\t{}'.format(
+        i18n.t('Calories'),
+        i18n.t('Fat'),
+        i18n.t('Carbs'),
+        i18n.t('Protein')))
+    strings.append('{:.0f}\t{:.2f}\t{:.2f}\t{:.2f}'.format(
+        calories_left,
+        fat_left,
+        carbs_left,
+        protein_left))
     update.message.reply_text("\n".join(strings))
