dbbe5d76abd9f1c888ed7765503351a59a7b4645
yakninja <andrey@yakninja.pro>
2021-10-09 19:34:04 +0200

    WIP

---
+++ alembic/versions/2021-10-09_73284d7b8325_daily_report.py
@@ -0,0 +1,24 @@
+"""daily_report
+
+Revision ID: 73284d7b8325
+Revises: 863875bf2677
+Create Date: 2021-10-09 19:03:59.922742
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '73284d7b8325'
+down_revision = '863875bf2677'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
